8d7ae4cccec9666ca33ef532f91eb958ab795e7e
lakshyaGiftkart <lakshya@giftkart.ai>
2024-12-15 21:14:40 +0530

    added cron to sync products of a store

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import CreateStoreBundles from "./bundle/createStoreBundles.js";
 import MigrateBundlesToShopify from "./bundle/migrateBundles.js";
-
+import SyncStoreProducts from "./bundle/syncStoreProducts.js";
 const StartJobs = () => {
   // CreateStoreBundles();
   MigrateBundlesToShopify();
+  // SyncStoreProducts();
 };
 StartJobs();

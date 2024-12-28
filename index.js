a6ef69895a1a0d1ec885a32dfeb39ca86294ccb4
lakshyaGiftkart <lakshya@giftkart.ai>
2024-12-28 18:14:14 +0530

    modied the index.js

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-// import CreateStoreBundles from "./jobs/bundle/createStoreBundles.js";
-// import MigrateBundlesToShopify from "./jobs/bundle/migrateBundlesMarketplace.js";
-// import SyncStoreProducts from "./jobs/products/syncStoreProducts.js";
-// import UpdateMedia from "./jobs/bundle/updateMedia.js";
+import MigrateBundlesToShopify from "./jobs/bundles/migrateBundlesMarketplace.js";
+import SyncStoreProducts from "./jobs/products/syncStoreProducts.js";
+import UpdateMedia from "./jobs/bundles/updateMedia.js";
 import MigrateProductsToBQ from "./jobs/products/migrateProductsBigQuery.js";
 const StartJobs = () => {
-  // CreateStoreBundles();
-  // MigrateBundlesToShopify();
-  // SyncStoreProducts();
-  // UpdateMedia();
+  MigrateBundlesToShopify();
+  SyncStoreProducts();
+  UpdateMedia();
   MigrateProductsToBQ();
 };
 StartJobs();

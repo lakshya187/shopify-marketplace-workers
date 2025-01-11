bd7be36db2847919c24aa561f4e58d85aa68b589
lakshyaGiftkart <lakshya@giftkart.ai>
2025-01-11 19:58:38 +0530

    enabled the migrate bundles to shopify

--- jobs/bundles/migrateBundlesMarketplace.js
+++ jobs/bundles/migrateBundlesMarketplace.js
@@ -125,7 +125,7 @@ const MigrateBundlesToShopify = async () => {
 };
 
 setInterval(() => {
-  // MigrateBundlesToShopify();
+  MigrateBundlesToShopify();
 }, process.env.MIGRATE_BUNDLE_WORKER_INTERVAL_MS);
 
 // utils
